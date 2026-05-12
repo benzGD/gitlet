9204ed022bdeefe44eeb126c8fe9680177cb367a
GD <GD@email.com>
2026-05-12 16:44:57 +0530

    original

---
+++ gitlet/Commit.java
@@ -0,0 +1,24 @@
+package gitlet;
+
+// TODO: any imports you need here
+
+/** Represents a gitlet commit object.
+ *  TODO: It's a good idea to give a description here of what else this Class
+ *  does at a high level.
+ *
+ *  @author TODO
+ */
+public class Commit {
+    /**
+     * TODO: add instance variables here.
+     *
+     * List all instance variables of the Commit class here with a useful
+     * comment above them describing what that variable represents and how that
+     * variable is used. We've provided one example for `message`.
+     */
+
+    /** The message of this Commit. */
+    private String message;
+
+    /* TODO: fill in the rest of this class. */
+}
